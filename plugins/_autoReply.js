7c2ae01ddc87b672ea70971637e7bda0c1216534
Arik9825 <108628057+Arik9825@users.noreply.github.com>
2024-01-09 22:05:04 +0600

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -2,10 +2,33 @@ export async function all(m) {
 
   // when someone sends a message to the bot's chat or group
   if ((m.mtype === 'personal' || m.isGroup) && !m.isBaileys && !m.isGroup) {
-    this.sendMessage(m.chat, { text: `Hello @${m.sender.split('@')[0]}\nI am currently busy and will reply when I am free.\n
-For urgent matters, please DM the owner.`.trim() }, { quoted: m });
-    m.react('💎');
+
+    // Get the current time
+    const currentTime = new Date().getTime();
+
+    // Get the last message time for this user
+    let lastMessageTime = this.db.data.users[m.sender].lastMessageTime || 0;
+
+    // Check if 30 minutes have passed since the last message
+    if (currentTime - lastMessageTime >= 1800000) {
+      // Send the message
+      this.sendMessage(
+        m.chat,
+        {
+          text: `Hello @${m.sender.split('@')[0]}\nI am currently busy and will reply when I am free.\nFor urgent matters, please DM the owner.`.trim()
+        },
+        { quoted: m }
+      );
+
+      // Update the last message time for this user
+      this.db.data.users[m.sender].lastMessageTime = currentTime;
+
+      // Save the updated database
+      this.db.save();
+    }
+
+    m.react('🤖');
   }
 
   return !0;
-}
+          }
